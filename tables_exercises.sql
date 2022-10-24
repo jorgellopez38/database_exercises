efb7cc23def810f90806a3526b82c8b4265fd98f
Jorge <jorge.l.lopez38@gmail.com>
2022-10-24 14:00:55 -0500

    adding tables_exercises

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -2,10 +2,10 @@ SHOW DATABASES;
 
 USE employees;
 SHOW TABLES;
-## 6. tables containing numeric type column are emp_no, birth_date, and hire_date
-## 7. tables containing string type column are first_name, last_name, and gender
-## 8. tables containing date type column are birth_date and hire_date
-## 9. they are part of the same database/schema
+## 6. all tables contain numeric type column
+## 7. all tables contain string type column
+## 8. tables containing date type column are dept_manager, employees, salaries, and titles
+## 9. they have no direct relationship
 /* 10. CREATE TABLE `dept_manager` (
   `emp_no` int NOT NULL,
   `dept_no` char(4) NOT NULL,
@@ -17,3 +17,4 @@ SHOW TABLES;
   CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 */
+
